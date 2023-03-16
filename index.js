d8fe082a6c6b1d6cfa1f7ffeade87e09d699549e
Sir-Lancelot-Du-Lac <lionel.feter@wp.pl>
2023-03-16 19:55:46 +0100

    Almost fully working chromium extension allowing to customize scrollbar

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 var tabActiveId = "";
 chrome.tabs.query({active: true}, function(tabs){tabActiveId = tabs[0].id});
+var isScrollColorModified = false;
 
 var cssColOne = (color) =>{
     return '\
-::--webkit-scrollbar-thumb{\
-    background-color:' + color + ';\
-}'};
+        ::-webkit-scrollbar-thumb{\
+        background-color:' + color + ';\
+    }'};
 
 const cssColSec = (color) =>{
-return '\
-::--webkit-scrollbar{\
-    background-color:'+ color +';\
-}'};
+    return '\
+        ::-webkit-scrollbar{\
+        background-color:'+ color +';\
+    }'};
+
+const cssWidth = (numb) =>{
+    return '\
+        ::-webkit-scrollbar{\
+        width:'+ numb +'px;\
+    }'};
+
+const cssRadius = (numb) =>{
+    return '\
+        ::-webkit-scrollbar-thumb{\
+        border-radius: '+ numb +'px;\
+    }'};
 
 const handleColorChange = (type) =>{
     console.log(type);
@@ -25,6 +38,7 @@ const handleColorChange = (type) =>{
             target:{tabId: tabActiveId},
             css: String(cssColOne(color)),
         });
+        if(!isScrollColorModified){handleMessager("Change second color to see effects")};
     }else{
         const color = document.getElementById("second").value;
         colorSec = color;
@@ -34,6 +48,7 @@ const handleColorChange = (type) =>{
             target:{tabId: tabActiveId},
             css: String(cssColSec(color)),
         });
+        isScrollColorModified = true;
     };
 };
 
@@ -41,9 +56,19 @@ const handleRoundNWidth = (type) =>{
 if(type){
     const rangeV = document.getElementById("round").value;
     console.log(rangeV);
+    chrome.scripting.insertCSS({
+        target:{tabId: tabActiveId},
+        css: String(cssRadius(rangeV)),
+    });
+    if(!isScrollColorModified){handleMessager("Change colors first!");};
 }else{
     const rangeV = document.getElementById("width").value;
     console.log(rangeV);
+    chrome.scripting.insertCSS({
+        target:{tabId: tabActiveId},
+        css: String(cssWidth(rangeV)),
+    });
+    if(!isScrollColorModified){handleMessager("Change colors first!");};
 }
 };
 
@@ -51,13 +76,24 @@ const handleCheckboxes = (type) =>{
     if(type==0){
         const radioV = document.getElementById("radioOne").value;
         console.log(radioV);
-        browser.scripting.insertCSS({code: "body{overflow: scroll;}"});
+        chrome.scripting.insertCSS({
+            target: {tabId: tabActiveId},
+            css: "body{overflow: scroll;}",
+            });
     }else if(type==1){
         const radioV = document.getElementById("radioSec").value;
         console.log(radioV);
-        browser.scripting.insertCSS({code: "body{overflow: hidden;}"});
+        chrome.scripting.insertCSS({
+            target: {tabId: tabActiveId},
+            css: "body{overflow: hidden;}",
+            });
     }
 };
+
+const handleMessager = (message) =>{
+    document.getElementById("information").innerHTML = message;
+
+}
 
 const handleListeners = () =>{
     console.log("Loaded")
